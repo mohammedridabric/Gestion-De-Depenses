f80aeb0f62fdd37e60b009bf4396c9d76bed2d64
Mohammed reda Bric <mohammedridabric@gmail.com>
2020-06-13 23:56:01 +0200

    first commit

---
+++ src/components/Deponses.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3 class="text-info"> Deponses </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Depones"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Gains.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3 class="text-info">Gians </h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gGains",
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
